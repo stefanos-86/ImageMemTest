0daf5aa61b598212c20158e6fef2e1c7590cbb38
stefanos-86 <stefanos-86@users.noreply.github.com>
2016-05-13 20:59:22 +0200

    Clean up the event system.

--- Events.py
+++ Events.py
@@ -10,15 +10,35 @@ class Event(object):
         """ Does nothing. """
         pass
 
-    def onKey(self):
-        print "base"
-        return False
+
+class DelayedEvent(Event):
+    """ This event has to be scheduled after a certain time. """
+    def __init__(self, gui, delay):
+        super(DelayedEvent, self).__init__(gui)
+        self.delay = delay
+
+    def register(self, back_to_scheduler):
+        self.gui.register_event(self.delay, back_to_scheduler)
 
 
-class BackgroundColorEvent(Event):
+class OnKeyEvent(Event):
+    """ This event waits for a key press. """
+    def __init__(self, gui, key):
+        super(OnKeyEvent, self).__init__(gui)
+        self.key = key
+
+    def register(self, back_to_scheduler):
+        self.gui.bind_key(self.key, back_to_scheduler)
+
+    def _remove_key_binding(self):
+        """ To be called at the end of happen() """  # TODO: consider a generic "unregister", but it makes no sense for DelayEvents.
+        self.gui.free_key(self.key)
+
+
+class BackgroundColorEvent(DelayedEvent):
     """ Orders the gui to change the background color. """
     def __init__(self, gui, rgb_triplet):
-        super(BackgroundColorEvent, self).__init__(gui)
+        super(BackgroundColorEvent, self).__init__(gui, 0)  # 0 Deleay: this is immediate.
         self.rgb_triplet = rgb_triplet
 
     def happen(self):
@@ -26,22 +46,13 @@ class BackgroundColorEvent(Event):
         self.gui.background_color(r, g, b)
 
 
-
-class PressAKeyToContinueEvent(Event):
+class PressAKeyToContinueEvent(OnKeyEvent):
     """ Does nothing until the user press the specified key. """
     def __init__(self, gui, key_to_wait):
-        super(PressAKeyToContinueEvent, self).__init__(gui)
-        self.key_to_wait = key_to_wait
-
-        #self.gui.bind_key(self.key_to_wait, self.happen)
-
-    def onKey(self):
-        print "derivato "
-        return True
+        super(PressAKeyToContinueEvent, self).__init__(gui, key_to_wait)
 
     def happen(self):
-        self.gui.free_key(self.key_to_wait)
-
+        self._remove_key_binding()
 
 
 class Scheduler():
@@ -57,26 +68,18 @@ class Scheduler():
         self.gui.register_event(0, self.schedule_event)  # Schedule immediately the 1st event of the list.
 
     def schedule_event(self):
-        print "--------sched"
         if self.event_iterator >= self.event_end:
             return  # Do nothing, we are at the end of the script.
 
-        kevent = self._current_event().onKey()
-        print kevent
-        if kevent:  # Apply polimorphism here...
-            self.gui.bind_key(self._current_event().key_to_wait, self.do_event)
-            print "on key"
-        else:
-            self.gui.register_event(0, self.do_event)
-            print "registered"
+        self._current_event().register(self.do_event)  # The event should not be called directly.
+                                                       # Its trigger must re-call the scheduler.
 
-    def do_event(self, event_from_gui=None):
-        print "--------do"
+    def do_event(self, event_from_gui=None):  # Event_from_gui is received in case of key presses.
         self._current_event().happen()  # Perform the scheduled action.
 
         self.event_iterator += 1        # Advance to the next.
 
-        self.schedule_event()  # Put another one in the pipe.
+        self.schedule_event()           # Put another one in the pipe.
 
 
     def _current_event(self):
