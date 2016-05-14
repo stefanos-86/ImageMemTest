e17f358afc92e97ece99956c9c68319fafb77449
stefanos-86 <stefanos-86@users.noreply.github.com>
2016-05-14 17:24:11 +0200

    Remove some unused code.

--- main.py
+++ main.py
@@ -2,40 +2,12 @@ import argparse
 
 import os
 
-from Experiment import Experiment
-
-#Includes above this line may be useless.
-from Events import ChangeBackgroundColor
-from Events import PressKeyToContinue
 from Events import Scheduler
 from GuiFacade import GuiFacade
 from ExperimentLoader import ExperimentLoader
 
 
-class ImageMemoryTestRunner(): #Deprecated
-    def __init__(self, experiment_file):
-
-
-        self.window = window
-        self.experiment = Experiment.from_file(os.path.abspath(experiment_file))
-
-        event_time_milliseconds = 0
-        for image in self.experiment.images:
-            panel = tk.Label(window, image=image.tk_image)
-            image.tk_panel = panel
-            window.after(event_time_milliseconds, image.emplace)
-            event_time_milliseconds += image.duration * 1000
-            window.after(event_time_milliseconds, image.remove)
-
-
-
-
-def main_loop(): # Unused, to be replaced by main()
-    experiment_file = parse_command_line()
-    imt = ImageMemoryTestRunner(experiment_file)
-    imt.start()
-
-def parse_command_line():
+def parse_command_line(): # TODO: may be eliminated once the GUI is used...
     parser = argparse.ArgumentParser()
     parser.add_argument('--experimentFile', help='path to the file with the experiment description', required=True)
     args = parser.parse_args()
