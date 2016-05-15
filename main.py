39a0281dd3dfdc0c0e20c9329749f7701f9fea68
stefanos-86 <stefanos-86@users.noreply.github.com>
2016-05-15 15:07:14 +0200

    More robust file selection.

--- main.py
+++ main.py
@@ -6,18 +6,27 @@ from Events import Scheduler
 from GuiFacade import GuiFacade
 from ExperimentLoader import ExperimentLoader
 
-def parse_command_line(): # TODO: may be eliminated once the GUI is used...
+
+def parse_command_line():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--experimentFile', help='path to the file with the experiment description', required=True)
+    parser.add_argument('-e', metavar="experiment_file",
+                        help='path to the file with the experiment description', required=False)
     args = parser.parse_args()
-    return args.experimentFile
+    return args.e
+
+
+def select_file():
+    filename = parse_command_line()
+    while filename is None or len(filename) == 0:  # Loop until we get something: we must have a file to read.
+        filename = gui.select_file()
+    return filename
 
 
 # Stupid trick to start the gui before doing work, to display error messages, if the need be, while initializing things.
 gui = GuiFacade()
 def start_experiment():
+    filename = select_file()
     loader = ExperimentLoader()
-    filename = gui.select_file()
     events = loader.load(filename, gui)
     scheduler = Scheduler(events, gui)
 
