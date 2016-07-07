2258328fb30cd02ab93dcdeeea89cee61f4722e8
stefanos-86 <stefanos-86@users.noreply.github.com>
2016-07-07 21:25:30 +0200

    Part 2 of the release script: demo files and zipping.

--- Release.py
+++ Release.py
@@ -1,21 +1,36 @@
 # Utility script to quickly create a release.
 import os
 import shutil
+import zipfile
+
 
 def my_position():
     return os.path.dirname(os.path.abspath(__file__))
 
+
 def release_subfolder():
     return  os.path.join(my_position(), "ImageMemoryTest")
+
 
 def delete_old_release():
     if os.path.isdir(release_subfolder()):
         shutil.rmtree(release_subfolder())
         print "Deleted old folder"
 
+
 def make_new_folder():
     os.mkdir(release_subfolder())
     print "Created new folder"
+
+
+def compress_folder():
+    zipf = zipfile.ZipFile('ImageMemoryTest.zip', 'w', zipfile.ZIP_DEFLATED)
+    for root, dirs, files in os.walk(release_subfolder()):
+        for file in files:
+            zipf.write(os.path.join(root, file))
+    zipf.close()
+    print "Compressed archive"
+
 
 def copy_files():
     release_content = [
@@ -32,15 +47,22 @@ def copy_files():
         "MemoryTest.bat",
         # Documentation
         "Instructions.pdf",
-        "License.txt"
+        "License.txt",
+        # Examples
+        "Demo"
     ]
     for file in release_content:
         source = os.path.abspath(file)
         destination = os.path.join(release_subfolder(), file)
         print "Copy " + source + " " + destination
-        shutil.copy(source, destination)
+        if os.path.isdir(source):
+            shutil.copytree(source, destination)
+        else:
+            shutil.copy(source, destination)
 
 if __name__ == "__main__":
     delete_old_release()
     make_new_folder()
     copy_files()
+    compress_folder()
+    # TODO: tag the repo?
